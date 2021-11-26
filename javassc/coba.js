99dbb933e6787f1d43252d0813ecebc6c87e8acf
andaraa <andikapradana140203@gmail.com>
2021-11-26 09:49:00 +0700

    commit

---
+++ javassc/coba.js
@@ -0,0 +1,55 @@
+// var jmlAngkot = 10;
+// var angkotBeroperasi = 6;
+// var noAngkot = 1;
+
+// while (noAngkot <= angkotBeroperasi) {
+//   console.log('angkot no ' + noAngkot + 'beroperasi dengan baik');
+
+//   noAngkot++;
+// }
+// for (noAngkot = 7; noAngkot <= jmlAngkot; noAngkot++);
+// console.log('angkot no ' + noAngkot + 'sedang rusak');
+
+// // variabel global yg gabisa ngintip ke dalam
+// var a =1;
+// function coba() {
+//   var b = 2;
+// }
+// coba();
+// console.log(a);
+
+// function jumlahDuaKubus(a, b) {
+//   volumeA = a * a * a;
+//   volumeB = b * b * b;
+
+//   total = volumeA + volumeB;
+//   return total;
+// }
+// alert(jumlahDuaKubus(8, 9));
+
+// ARRAY
+// var arr = ['andika', 'pradana', 'andara'];
+// console.log(arr);
+
+// bisa juga
+
+// var arr = [];
+// arr[0] = 'andika';
+// arr[1] = 'pradana';
+// arr[2] = 'andara';
+// console.log(arr);
+
+// key & value pair
+
+// var binatang = ['kambing', 'bandot', 'rusa', 'domba'];
+// console.log(binatang);
+
+// IF & ELSE;
+// var angka = prompt('masukan angka anda');
+// if (angka % 2 == 0) {
+//   alert('adalah bilangan genap');
+// } else (angka %2 ==1) {
+//   alert('bilangan ganjil');
+// }
+// var mobil = ['bmw', 'mercedes', 'bugatti', 'pagani'];
+// console.log(mobil.length);
